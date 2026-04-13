343c02704cbcfbb14dee48c7bcfd1c2bf834140f
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-04-13 10:47:57 -0700

    completed up to ArticleCard cshtml file

---
+++ COMP003B.LectureActivity4/Models/Article.cs
@@ -0,0 +1,17 @@
+namespace COMP003B.LectureActivity4.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Article
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [StringLength(100)]
+    public string Title { get; set; }
+    
+    [Required]
+    [StringLength(300)]
+    public string Summary { get; set; }
+    
+}
